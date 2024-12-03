3f0d01b8748c3088c9a2b3fa67b07fbec91584b5
Kyeongran <eggking2580@gmail.com>
2024-12-03 17:41:11 +0900

    ToDo Entity class initialized

--- src/main/java/com/example/todo/dto/ToDoRequestDto.java
+++ src/main/java/com/example/todo/dto/ToDoRequestDto.java
@@ -1,5 +1,8 @@
 package com.example.todo.dto;
 
+/**
+ *
+ */
 public class ToDoRequestDto {
-
+    private
 }
--- src/main/java/com/example/todo/entity/ToDo.java
+++ src/main/java/com/example/todo/entity/ToDo.java
@@ -3,6 +3,21 @@ package com.example.todo.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * 일정 관리 애플리케이션의 ToDo Entity 클래스
+ * 이 클래스는 데이터베이스의 ToDo 테이블과 매핑되며 일정 데이터를 표현한다.
+ *
+ * <p>
+ *     - id : 일정의 고유 식별자
+ *     - title : 일정의 제목
+ *     - contentes : 일정의 내용
+ * </p>
+ *
+ * Lombok 애너테이션
+ * - @Getter : 모든 필드에 대한 Getter 메서드를 자동 생성
+ * - @AllArgsConstructor : 모든 필드를 초기화하는 생성자를 자동 생성
+ */
+
 @Getter
 @AllArgsConstructor
 public class ToDo {
